b0867bac01db27d169f0ee59f9ba6e93c17f0c63
zigmajarvis <pay.alan.mel@gmail.com>
2023-11-27 16:04:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <a
+        <a
           className="App-link"
           href="https://whatsapp.com/channel/0029Va9s8pH17Emp5rx6Rc15"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Project Lily
-        </a> */}
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
         <p>Never Settle Persue Virtue.</p>
       </header>
     </div>
