4a32b911726a32a27257b42becf688366ab3bd58
zigmajarvis <pay.alan.mel@gmail.com>
2023-11-27 15:07:29 +0530

    add react web app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./PL.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <a
+          className="App-link"
+          href="https://whatsapp.com/channel/0029Va9s8pH17Emp5rx6Rc15"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Project Lily
+        </a> */}
+        <p>Never Settle Persue Virtue.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
